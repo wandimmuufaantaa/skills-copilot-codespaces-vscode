8a6aebf2f098fff00134827068fd441e74e36bf3
wandimmuufaantaa <133769072+wandimmuufaantaa@users.noreply.github.com>
2023-07-17 22:51:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,25 +1,54 @@
-// Create a web server
-// http://localhost:3000
+// Create web server
 const express = require('express');
 const app = express();
+const port = 3000;
 
-const comments = require('./comments.json');
-
-app.get('/', (req, res) => {
-    res.send('Welcome to our API');
+// Create a route that will display a list of comments
+app.get('/comments', (request, response) => {
+    response.send('List of comments');
 });
 
-app.get('/comments', (req, res) => {
-    res.json(comments);
+// Create a route that will display a single comment
+app.get('/comments/:id', (request, response) => {
+    const id = request.params.id;
+    response.send(`Comment ${id}`);
 });
 
-app.get('/comments/:id', (req, res) => {
-    const comment = comments.find((comment) => comment.id === parseInt(req.params.id));
-    if (!comment) {
-        res.status(404).send('The comment with the given ID was not found');
-    }
-    res.json(comment);
+// Create a route that will display a list of comments for a post
+app.get('/posts/:postId/comments', (request, response) => {
+    const postId = request.params.postId;
+    response.send(`Comments for post ${postId}`);
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+// Create a route that will display a form for creating a comment
+app.get('/comments/new', (request, response) => {
+    response.send('Form for creating a comment');
+});
+
+// Create a route that will display a form for editing a comment
+app.get('/comments/:id/edit', (request, response) => {
+    const id = request.params.id;
+    response.send(`Form for editing comment ${id}`);
+});
+
+// Create a route that will delete a comment
+app.delete('/comments/:id', (request, response) => {
+    const id = request.params.id;
+    response.send(`Delete comment ${id}`);
+});
+
+// Create a route that will create a comment
+app.post('/comments', (request, response) => {
+    response.send('Create comment');
+});
+
+// Create a route that will update a comment
+app.put('/comments/:id', (request, response) => {
+    const id = request.params.id;
+    response.send(`Update comment ${id}`);
+});
+
+// Start the web server
+app.listen(port, () => {
+    console.log(`Web server running at http://localhost:${port}`);
+});
